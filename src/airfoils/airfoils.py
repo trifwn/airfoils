d52d53f7e116fee37c8dd7ebc4028a62478c9f4a
trifwn <tryfonthem@gmail.com>
2024-03-22 20:26:50 +0200

    Fixed State of interpolator obj

--- src/airfoils/airfoils.py
+++ src/airfoils/airfoils.py
@@ -59,29 +59,59 @@ class Interpolator(interp1d):
         :fill_value: (str or float) If a string, it must be one of 'extrapolate', 'constant', 'nearest', 'zero', 'slinear', 'quadratic', or 'cubic'
     """
 
-    def __init__(
-        self, x, y, kind="linear", bounds_error=False, fill_value="extrapolate"
-    ):
-        self.given_x = x
-        self.given_y = y
-        self.given_kind = kind
-        self.given_bounds_error = bounds_error
-        self.given_fill_value = fill_value
+    def __getstate__(self):
+        """
+        This method defines the state to be pickled for interp1d objects.
 
-        super().__init__(
-            x, y, kind=kind, bounds_error=bounds_error, fill_value=fill_value
-        )
+        We exclude the private member '_InterpolatorBase__spline' as it's
+        recreated during object initialization.
 
-    def __setstate__(self, state) -> None:
-        print(type(state))
-        x, y, kind, bounds_error, fill_value = state
-        super().__init__(
-            x, y, kind=kind, bounds_error=bounds_error, fill_value=fill_value
+        Returns:
+            dict: A dictionary containing the picklable state of the object.
+        """
+        return {
+            "kind": self._kind,
+            "bounds_error": self.bounds_error,
+            "fill_value": self.fill_value,
+            # "assume_sorted": self.assume_sorted,
+            "copy": self.copy,
+            "x": self.x.copy(),
+            "y": self.y.copy(),
+            # "args": self.args,
+            # "kwargs": self.kwargs,
+        }
+
+    def __setstate__(self, state):
+        """
+        This method reconstructs the interp1d object from the pickled state.
+
+        Args:
+            state (dict): The pickled state of the object.
+        """
+        self.kind = state["kind"]
+        self.bounds_error = state["bounds_error"]
+        self.fill_value = state["fill_value"]
+        # self.assume_sorted = state["assume_sorted"]
+        self.copy = state["copy"]
+        self.xi = state["x"]
+        self.yi = state["y"]
+        # self.args = state["args"]
+        # self.kwargs = state["kwargs"]
+        # Recreate the spline object during initialization
+        self.__init__(
+            self.xi,
+            self.yi,
+            kind=self.kind,
+            bounds_error=self.bounds_error,
+            fill_value=self.fill_value,
+            # assume_sorted=self.assume_sorted,
+            copy=self.copy,
+            # args=self.args,
+            # kwargs=self.kwargs,
         )
 
 
 class Airfoil:
-
     def __init__(self, upper, lower):
         """
         Main constructor method
@@ -129,7 +159,6 @@ class Airfoil:
             bounds_error=False,
             fill_value="extrapolate",
         )
-
         self._y_lower_interp = Interpolator(
             self._x_lower,
             self._y_lower,
